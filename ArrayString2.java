4359a58493a278d45f23d3051238599cb18677a8
kms7770523 <audtj0523@g.shingu.ac.kr>
2024-11-05 11:41:01 +0900

    2024143037 김명서

---
+++ ArrayString2.java
@@ -0,0 +1,10 @@
+package day8_array;
+
+public class ArrayString2 {
+
+	public static void main(String[] args) {
+		String[] strArr = {"프로그래밍", "완전", "너무", "싫어", "노는게", "좋아요", "별로"};
+		System.out.printf("%s %s %s!", strArr[0], strArr[1], strArr[5]);
+	}
+
+}
