4cab6a2842cff7cc944005cc3f92d6ecc4f3eb02
kms7770523 <audtj0523@g.shingu.ac.kr>
2024-11-05 12:21:18 +0900

    Add files via upload

---
+++ ArrayQuiz.java
@@ -0,0 +1,10 @@
+package day8_array;
+
+public class ArrayQuiz {
+
+	public static void main(String[] args) {
+		String[] strarr = {"자바", "완전", "쉬워","어려워","좋아","싫어"};
+        System.out.printf("%s %s %s \n",strarr[0], strarr[1], strarr[5]);
+	}
+
+}
